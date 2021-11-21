87d927df76c50552c01250be959a0d52becca5be
bert <herbertmatos10@hotmail.com>
2021-11-21 20:38:23 -0300

    feat: melhoria no middleware errorHandler

--- src/app/middlewares/errorHandler.js
+++ src/app/middlewares/errorHandler.js
@@ -1,5 +1,8 @@
 const errorHandler = (error, req, res, next) => {
   console.log(error);
+  if (error.status === 401) {
+    return res.status(error.status).json({ message: error.message || error.error });
+  }
   res.sendStatus(500);
 };
 
